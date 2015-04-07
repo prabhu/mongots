bb3b949296bab448cd74703f8a017bb315b2a30e
Prabhu Subramanian <prabhu@colearnr.com>
2015-04-07 17:45:13 +0000

    First Commit

---
+++ GruntFile.js
@@ -0,0 +1,26 @@
+﻿module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        
+        typescript: {
+            base: {
+                src: ['lib/**/*.ts'],
+                dest: 'dist/mongots.js',
+                options: {
+                    module: 'amd',
+                    target: 'es5'
+                }
+            }
+        },
+        watch: {
+            files: '**/*.ts',
+            tasks: ['typescript']
+        }
+    });
+    
+    grunt.registerTask('default', ['typescript']);
+ 
+}
---
+++ index.ts
@@ -0,0 +1,45 @@
+﻿declare function require(name: string);
+var thunky = require('thunky');
+var toMongodbCore = require('to-mongodb-core');
+var parse = require('parse-mongo-url');
+
+import Database = require('./lib/database');
+var getTopology = require('./lib/get-topology');
+
+var getDbName = function (connString: any): string {
+  if (typeof connString !== 'string') {
+    return null;
+  }
+  var config = parse(connString);
+  return config.dbName;
+};
+
+var init = function(connString: any, cols: Array<string>): Database {
+  var dbname = getDbName(connString);
+  var onserver = thunky(function (cb: any) {
+    getTopology(connString, function (err: any, topology: any) {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, topology);
+    });
+  });
+
+  if (!dbname) {
+    dbname = connString._dbname;
+    onserver = thunky(function (cb: any) {
+      toMongodbCore(connString, function (err: any, server: any) {
+        if (err) {
+          cb(new Error('You must pass a connection string or a mongojs instance.'));
+        } else {
+          cb(null, server);
+        }
+      });
+    });
+  }
+
+  var that = new Database({ name: dbname, cols: cols }, onserver);
+  return that;
+};
+
+export = init;
