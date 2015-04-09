fd899ee46d4d28953d4184659baeaaca625bdbeb
Prabhu Subramanian <prabhu@colearnr.com>
2015-04-09 23:52:00 +0100

    Skip admin db tests on ci

--- src/test/test-max-time.js
+++ src/test/test-max-time.js
@@ -2,20 +2,23 @@ var insert = require('./insert');
 var mongots = require('../../dist/index');
 var admindb = mongots('admin');
 
-insert('maxTimeMS test', [{
-  hello:'world'
-}], function(db, t, done) {
-	admindb.runCommand({configureFailPoint: "maxTimeAlwaysTimeOut", mode: "alwaysOn"}, function(err, result) {
-	  db.a.find({"$where": "sleep(100) || true"}).maxTimeMS(10, function(err, docs) {
-	    t.ok(err != null);
-	    admindb.runCommand({configureFailPoint: "maxTimeAlwaysTimeOut", mode: "off"}, function(err, result) {
-	    	db.a.find().maxTimeMS(10, function(err, docs){
-			    t.equal(docs.length, 1);
-			    t.equal(docs[0].hello, 'world');
-			    done();
-		    });
-		  });
-	  });
-	});
+var SKIP_ADMIN_TESTS = process.env.SKIP_ADMIN_TESTS;
 
-});
+if (!SKIP_ADMIN_TESTS) {
+	insert('maxTimeMS test', [{
+	  hello:'world'
+	}], function(db, t, done) {
+		admindb.runCommand({configureFailPoint: "maxTimeAlwaysTimeOut", mode: "alwaysOn"}, function(err, result) {
+		  db.a.find({"$where": "sleep(100) || true"}).maxTimeMS(10, function(err, docs) {
+		    t.ok(err != null);
+		    admindb.runCommand({configureFailPoint: "maxTimeAlwaysTimeOut", mode: "off"}, function(err, result) {
+		    	db.a.find().maxTimeMS(10, function(err, docs){
+				    t.equal(docs.length, 1);
+				    t.equal(docs[0].hello, 'world');
+				    done();
+			    });
+			  });
+		  });
+		});
+	});
+}
